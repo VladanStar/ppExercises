9fed40d5e393e1fb44d8c2a9c39b905d0b354c19
VladanStar <vladancupric@gmail.com>
2021-03-17 09:40:03 +0100

    Add new Task 18

--- Loops/loopsExercises.js
+++ Loops/loopsExercises.js
@@ -104,7 +104,7 @@ var result = "";
 
 for (var i = 0; i < str2.length; i++) {
     if (str2[i] == "y") {
-       result = "yes";
+        result = "yes";
     }
     else {
         result = "no";
@@ -121,10 +121,10 @@ console.log(result);
 // Example output:
 // 24
 let n2 = 4; //  4 * 3 * 2 * 1 = 24
-var p =1;
+var p = 1;
 // *********** YOUR CODE HERE **********************
 
-for(var i = 1; i<=n2; i++){
+for (var i = 1; i <= n2; i++) {
     p *= i;
 }
 console.log(p);
@@ -181,15 +181,163 @@ console.log(true);
 // Write a program that finds the summation of every number from 1 to num. 
 // The number will always be a positive integer greater than 0.
 let num1 = 8;
-var sum =0;
-var my_sum =0;
+var sum = 0;
+var my_sum = 0;
 // Example output:
 // 1 + 2 = 3
 // 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 = 36 
 
 // *********** YOUR CODE HERE **********************
 
-for(var i =0; i<=num1;i++){
-    my_sum = my_sum+i;
+for (var i = 0; i <= num1; i++) {
+    my_sum = my_sum + i;
 }
 console.log(my_sum);
+
+
+// ********************** Exercise 11 STARTS HERE **********************
+// 11. Write a for loop that will iterate from 0 to 15. For each iteration, it will check if the current
+//number is odd or even, and display a message to the screen.
+
+// *********** YOUR CODE HERE **********************
+
+for (var i = 0; i <= 15; i++) {
+    if (i % 2 == 0) {
+        console.log(i + " is even");
+    }
+    else {
+        console.log(i + " is odd");
+    }
+}
+
+
+// ********************** Exercise 12 STARTS HERE **********************
+//Write a program to sum the multiples of 3 and 5 under 1000.
+
+// *********** YOUR CODE HERE **********************
+var sum = 0;
+for (var i = 0; i < 1000; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        sum += i;
+    }
+}
+console.log(sum);
+
+// ********************** Exercise 13 STARTS HERE **********************
+//Write a program to compute the sum and product of an array of integers.
+// *********** YOUR CODE HERE **********************
+var num1 = 10;
+var sum = 0;
+var product = 1;
+for (var i = 1; i < num1; i++) {
+    sum += i;
+    product *= i;
+}
+console.log(sum);
+console.log(product);
+
+
+// ********************** Exercise 14 STARTS HERE **********************
+/* 4. Write a program which prints the elements of the following array as a single string.
+*/
+var x = ["1", "A", "B", "c", "r", true, NaN, undefined];
+var s = "";
+// *********** YOUR CODE HERE **********************
+
+for (var i = 0; i < x.length; i++) {
+    s += x[i];
+}
+console.log(s);
+
+// ********************** Exercise 15 STARTS HERE **********************
+/* 
+ Write a program that prints the elements of the following array.
+var a = [
+[1, 2, 1, 24],
+[8, 11, 9, 4],
+[7, 0, 7, 27]
+];
+*/
+// *********** YOUR CODE HERE **********************
+var a = [
+
+    [1, 2, 1, 24],
+    [8, 11, 9, 4],
+    [7, 0, 7, 27]
+];
+var b = [];
+for (var i in a) {
+    for (var j in a[i]) {
+        b[b.length] = a[i][j];
+    }
+}
+console.log(b);
+
+// ********************** Exercise 16 STARTS HERE **********************
+/* 
+16. Write a program that outputs the sum of squares of the first 20 numbers.
+*/
+// *********** YOUR CODE HERE **********************
+
+var sum = 0;
+for (var i = 0; i <= 20; i++) {
+    sum += (i * i);
+}
+console.log(sum);
+
+// ********************** Exercise 17 STARTS HERE **********************
+/* 
+Write a program that computes average marks of the following students. Then use this
+average to determine the corresponding grade.
+*/
+// *********** YOUR CODE HERE **********************
+var students = [['David', 80], ['Marko', 77], ['Dany', 88], ['John', 95], ['Thomas', 68]];
+
+var Avgmarks = 0;
+var sum = 0;
+for (var i = 0; i < students.length; i++) {
+    sum += students[i][1];
+}
+Avgmarks = sum / students.length;
+console.log(Avgmarks);
+if (avg < 60) {
+    console.log("Grade : F");
+}
+else if (avg <= 70) {
+    console.log("Grade : D");
+}
+else if (avg <= 80) {
+    console.log("Grade : C");
+}
+else if (avg <= 90) {
+    console.log("Grade : B");
+}
+else if (avg <= 100) {
+    console.log("Grade : A");
+}
+// ********************** Exercise 18 STARTS HERE **********************
+/* 
+18. Write a program that uses console.log to print all the numbers from 1 to 100, with two
+exceptions. For numbers divisible by 3, print "Fizz"; instead of the number, and for numbers
+divisible by 5 (and not 3), print "Buzz"; instead. When you have that working, modify your
+program to print "FizzBuzz";, for numbers that are divisible by both 3 and 5 (and still print
+"Fizz"; or "Buzz"; for numbers divisible by only one of those).
+*/
+// *********** YOUR CODE HERE **********************
+
+for (var i = 0; i < 100; i++) {
+    var divThree = i % 3;
+    var divFive = i % 5;
+    if (divThree == 0 && divFive == 0) {
+        console.log("FizzBuzb");
+    }
+    else if (divThree == 0) {
+        console.log("Fizz");
+    }
+    else if (divFive == 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(i);
+    }
+}
