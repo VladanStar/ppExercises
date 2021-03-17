76bd336c120cedf5f27339aecbc6814df08c534f
VladanStar <vladancupric@gmail.com>
2021-03-17 07:48:14 +0100

    Add new task

--- Loops/loopsExercises.js
+++ Loops/loopsExercises.js
@@ -9,37 +9,37 @@ process.stdin.setEncoding('utf8');
 // Get the sum of two arrays...actually the sum of all their elements. 
 // P.S. Each array includes only integer numbers. 
 // Example input arrays:
-var arr_1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; // --> 276
-var arr_2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26]; // --> 351
+var arr_1 = [3, 5, 22, 5, 7, 2, 45, 75, 89, 21, 2]; // --> 276
+var arr_2 = [9, 2, 42, 55, 71, 22, 4, 5, 90, 25, 26]; // --> 351
 // Example output: 
 // 627
 
 // *********** YOUR CODE HERE **********************
-var sum=0;
-for(var i =0; i<arr_1.length;i++){
-    sum +=arr_1[i];
+var sum = 0;
+for (var i = 0; i < arr_1.length; i++) {
+    sum += arr_1[i];
 }
- console.log(sum);
- var sum2 =0;
-for(var i =0;i<arr_2.length;i++){
-    sum2+=arr_2[i];
+console.log(sum);
+var sum2 = 0;
+for (var i = 0; i < arr_2.length; i++) {
+    sum2 += arr_2[i];
 }
 console.log(sum2);
-var result = sum+sum2;
+var result = sum + sum2;
 console.log(result);
 
 
 
 // ********************** Exercise 2 STARTS HERE **********************
 // Using a for loop print all even numbers up to and including n. Don’t include 0.  
-let n1 = 22; 
+let n1 = 22;
 // Example output: 
 // 2 4 6 8 10 12 14 16 18 20 22 OR each item on a new line
 
 // *********** YOUR CODE HERE **********************
 
-for (var i =0; i<=n1;i++){
-    if(i %2 ==0){
+for (var i = 0; i <= n1; i++) {
+    if (i % 2 == 0) {
         console.log(i);
     }
 }
@@ -54,66 +54,80 @@ var arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
 
 // *********** YOUR CODE HERE **********************
 
-for (var i = arr.length-1; i >= 0; i--) {  
-  console.log(arr[i]);  
-}  
+for (var i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i]);
+}
 
 
 // ******* 
 //*************** Exercise 4 STARTS HERE **********************
 // Given two arrays of integers. Add up each element in the same position and create a new array containing the sum of each pair. Assume both arrays are of the same length. 
-var arr_3   = [4, 6, 7];
-var arr_4	= [8, 1, 9];
-var arr_5 =[];
+var arr_3 = [4, 6, 7];
+var arr_4 = [8, 1, 9];
+var arr_5 = [];
 // Example output: 
 // [12, 7, 16]
 
 // *********** YOUR CODE HERE **********************
-for(var i =0; i<arr_3.length;i++){
-    arr_5[i]=  arr_3[i] + arr_4[i];
+for (var i = 0; i < arr_3.length; i++) {
+    arr_5[i] = arr_3[i] + arr_4[i];
 }
 console.log(arr_5);
 
 // ********************** Exercise 5 STARTS HERE **********************
 // Given a string change the every second letter to an uppercase  ‘Z’. Assume there are no space. 
-let str1 = "javascript";  
+var str1 = "javascript";
+var a = "Z";
+var str2 = [];
 // Example output: 
 // jZvZsZrZpZ OR each letter on a new line
 
 // *********** YOUR CODE HERE **********************
-
-
-
-
-
-
-
+for (var i = 0; i < str1.length; i++) {
+    if (i % 2 !== 0 && i != 0) {
+        console.log(str2[str2.length] = "Z");
+    }
+    else {
+        console.log(str2[str2.length] = str1[i]);
+    }
+}
 
 // ********************** Exercise 6 STARTS HERE **********************
 // Check if a string contains the letter “y”. Print “yes” if it does and “no” if it does not. 
-let str2 = "don’t know why";
+var str2 = "don’t know why";
+var result = "";
 // Example output: 
 // “yes” 
 
 // *********** YOUR CODE HERE **********************
 
 
-
-
+for (var i = 0; i < str2.length; i++) {
+    if (str2[i] == "y") {
+       result = "yes";
+    }
+    else {
+        result = "no";
+    }
+}
+console.log(result);
 
 
 
 
 // ********************** Exercise 7 STARTS HERE **********************
 // Given a number n Calculate the factorial of the number 
-let n2 = 4; //  4 * 3 * 2 * 1 = 24
+
 // Example output:
 // 24
-
+let n2 = 4; //  4 * 3 * 2 * 1 = 24
+var p =1;
 // *********** YOUR CODE HERE **********************
 
-
-
+for(var i = 1; i<=n2; i++){
+    p *= i;
+}
+console.log(p);
 
 
 
@@ -123,22 +137,17 @@ let n2 = 4; //  4 * 3 * 2 * 1 = 24
 // Write a program that will allow someone to guess a four digit pin exactly 4 times. If the user guesses the number correctly. It prints  “That was correct!” Otherwise it will print “Sorry that was wrong.” Program stops after the 4th attempt of if they got it right. 
 let pin = 0704;
 // Example output:
-// Please make your guess: 
-// 4544 
-// Sorry that was wrong.
+//Sorry that was wrong.
 // Please make your guess: 
 // 4444
 // Sorry that was wrong.
 // Please make your guess: 
 // 0704
-// That was correct!
+// That was correct! Please make your guess: 
+// 4544 
+// So
 
 // *********** YOUR CODE HERE **********************
-
-
-
-
-
 
 
 
@@ -155,22 +164,32 @@ let str4 = "Java";
 
 // *********** YOUR CODE HERE **********************
 
-
-
-
-
-
-
+var length = str3.length;
+var forward = 0;
+var backward = length - 1;
+while (backward > forward) {
+    var forwardChar = str3.charAt(forward++);
+    var backwardChar = str3.charAt(backward--);
+    if (forwardChar != backwardChar)
+        console.log(false);
+}
+console.log(true);
 
 
 // ********************** Exercise 10 STARTS HERE **********************
 // This is a code wars kata. click here to train on “Grasshopper – Summation” on code wars.
 // Write a program that finds the summation of every number from 1 to num. 
 // The number will always be a positive integer greater than 0.
-let num1 = 2; 
-let num2 = 8; 
-// Example output: 
+let num1 = 8;
+var sum =0;
+var my_sum =0;
+// Example output:
 // 1 + 2 = 3
 // 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 = 36 
 
 // *********** YOUR CODE HERE **********************
+
+for(var i =0; i<=num1;i++){
+    my_sum = my_sum+i;
+}
+console.log(my_sum);
