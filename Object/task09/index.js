c3c1cd9c668921141cf5bd59e6d6fe19893a5ffa
VladanStar <vladancupric@gmail.com>
2021-05-11 22:29:53 +0200

    New object intro with this

--- Object/task09/index.js
+++ Object/task09/index.js
@@ -1,3 +1,12 @@
+/*4. Write a function that creates an object that represents a culinary recipe. Each recipe is
+described by: name, type of cuisine, complexity (value from 1 to 5), list of ingredients,
+preparing time, preparing instruction.
+○ Add a method that prints out all the ingredients necessary for the meal
+preparation.
+○ Add a method that checks if a meal can be prepared for 15 minutes.
+○ Add a method that changes the type of cuisine to the given value.
+○ Add a method that delete a given ingredient from the list of ingredients.*/
+
 function CulinareRecipe(
   name,
   type,
@@ -16,11 +25,24 @@ function CulinareRecipe(
     return (this.type = cuisine);
   };
   this.is15M = function (preparingTime) {
-    var isTime = preparingTime <= 15 ? "Yess" : "No";
+    var isTime = this.reparingTime <= 15 ? "Yess" : "No";
     return isTime;
   };
-  this.removeI = function (ingredient) {};
+  this.deleteIngredients = function (ingredient) {
+    var changedListOfIngredients = [];
+    for (var i = 0; i < this.listOfIngrendients.length; i++) {
+      if (this.listOfIngrendients[i] != ingredient) {
+        changedListOfIngredients[changedListOfIngredients.length] =
+          this.listOfIngrendients[i];
+      } else {
+        continue;
+      }
+    }
+
+    return (this.listOfIngredients = changedListOfIngredients);
+  };
 }
+
 var piza = new CulinareRecipe(
   "Quatro Staqione",
   "Italy",
@@ -38,7 +60,8 @@ var piza = new CulinareRecipe(
   "In the colinary"
 );
 piza.change("Juzna Srbija");
-piza.removeI("masline");
+
+piza.deleteIngredients("masline");
 piza.preparingTime = 22;
 console.log(piza.is15M(22));
 
