d94bd6079daeade4c49bb7d1651836fc1b13126f
VladanStar <vladancupric@gmail.com>
2021-05-21 11:56:54 +0200

    builtIn Objects Strings all done

--- BuiltInObjects/builtInObjectsString/builtInObjectString.js
+++ BuiltInObjects/builtInObjectsString/builtInObjectString.js
@@ -1,29 +1,209 @@
-var a = 'a'
-var b = 'b'
-if (a < b) { // true
-  console.log(a + ' is less than ' + b)
+var a = "a";
+var b = "b";
+if (a < b) {
+  // true
+  console.log(a + " is less than " + b);
 } else if (a > b) {
-  console.log(a + ' is greater than ' + b)
+  console.log(a + " is greater than " + b);
 } else {
-  console.log(a + ' and ' + b + ' are equal.')
+  console.log(a + " and " + b + " are equal.");
 }
 
-function isEqual(str1, str2)
-{
-    return str1.toUpperCase() === str2.toUpperCase()
+function isEqual(str1, str2) {
+  return str1.toUpperCase() === str2.toUpperCase();
 } // isEqual
 
-let s1 = '2 + 2'              // creates a string primitive
-let s2 = new String('2 + 2')  // creates a String object
-console.log(eval(s1))         // returns the number 4
-console.log(eval(s2))         // returns the string "2 + 2"
+let s1 = "2 + 2"; // creates a string primitive
+let s2 = new String("2 + 2"); // creates a String object
+console.log(eval(s1)); // returns the number 4
+console.log(eval(s2)); // returns the string "2 + 2"
 
-console.log(eval(s2.valueOf()))  // returns the number 4
+console.log(eval(s2.valueOf())); // returns the number 4
 
-let longString = "This is a very long string which needs " +
-                 "to wrap across multiple lines because " +
-    "otherwise my code is unreadable."
-                 
-let longString = "This is a very long string which needs \
+var longString =
+  "This is a very long string which needs " +
+  "to wrap across multiple lines because " +
+  "otherwise my code is unreadable.";
+
+var longString =
+  "This is a very long string which needs \
 to wrap across multiple lines because \
-otherwise my code is unreadable."    
+otherwise my code is unreadable.";
+
+let outputStrings = [];
+var inputValues = "dragan ilic";
+for (let i = 0, n = inputValues.length; i < n; ++i) {
+  outputStrings.push(String(inputValues[i]));
+}
+console.log(outputStrings);
+
+/*! https://mths.be/codepointat v0.2.0 by @mathias */
+if (!String.prototype.codePointAt) {
+  (function () {
+    "use strict"; // needed to support `apply`/`call` with `undefined`/`null`
+    var defineProperty = (function () {
+      // IE 8 only supports `Object.defineProperty` on DOM elements
+      try {
+        var object = {};
+        var $defineProperty = Object.defineProperty;
+        var result = $defineProperty(object, object, object) && $defineProperty;
+      } catch (error) {}
+      return result;
+    })();
+    var codePointAt = function (position) {
+      if (this == null) {
+        throw TypeError();
+      }
+      var string = String(this);
+      var size = string.length;
+      // `ToInteger`
+      var index = position ? Number(position) : 0;
+      if (index != index) {
+        // better `isNaN`
+        index = 0;
+      }
+      // Account for out-of-bounds indices:
+      if (index < 0 || index >= size) {
+        return undefined;
+      }
+      // Get the first code unit
+      var first = string.charCodeAt(index);
+      var second;
+      if (
+        // check if it’s the start of a surrogate pair
+        first >= 0xd800 &&
+        first <= 0xdbff && // high surrogate
+        size > index + 1 // there is a next code unit
+      ) {
+        second = string.charCodeAt(index + 1);
+        if (second >= 0xdc00 && second <= 0xdfff) {
+          // low surrogate
+          // https://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
+          return (first - 0xd800) * 0x400 + second - 0xdc00 + 0x10000;
+        }
+      }
+      return first;
+    };
+    if (defineProperty) {
+      defineProperty(String.prototype, "codePointAt", {
+        value: codePointAt,
+        configurable: true,
+        writable: true,
+      });
+    } else {
+      String.prototype.codePointAt = codePointAt;
+    }
+  })();
+}
+
+var sentence = "The quick brown fox jumps over the lazy dog.";
+
+var word = "fox";
+
+console.log(
+  `The word "${word}" ${
+    sentence.includes(word) ? "is" : "is not"
+  } in the sentence`
+);
+// expected output: "The word "fox" is in the sentence"
+console.log(sentence.includes());
+
+var str = "To be, or not to be, that is the question.";
+var count = 0;
+var position = str.indexOf("e");
+
+while (position !== -1) {
+  count++;
+  position = str.indexOf("e", position + 1);
+}
+
+console.log(count); // displays 4
+// mozemo da izbrojimo koliko se puta ponavlja neko slovo, preko indexOf() i while petlje.
+
+const paragraph = 'The quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?';
+
+const searchTerm = 'dog';
+
+console.log(`The index of the first "${searchTerm}" from the end is ${paragraph.lastIndexOf(searchTerm)}`);
+// expected output: "The index of the first "dog" from the end is 52"
+console.log(paragraph.lastIndexOf(searchTerm, 42));
+// index do koje pozicije u stringu ga trazi, vraca -1 ako ga ne nadje.
+
+const str = 'For more information, see Chapter 3.4.5.1';
+const re = /see (chapter \d+(\.\d)*)/i;
+const found = str.match(re);
+
+console.log(found);
+
+// logs [ 'see Chapter 3.4.5.1',
+//        'Chapter 3.4.5.1',
+//        '.1',
+//        index: 22,
+//        input: 'For more information, see Chapter 3.4.5.1' ]
+
+// 'see Chapter 3.4.5.1' is the whole match.
+// 'Chapter 3.4.5.1' was captured by '(chapter \d+(\.\d)*)'.
+// '.1' was the last value captured by '(\.\d)'.
+// The 'index' property (22) is the zero-based index of the whole match.
+// The 'input' property is the original string that was parsed.
+
+const str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+const regexp = /[A-E]/gi;
+const matches_array = str.match(regexp);
+
+console.log(matches_array);
+// ['A', 'B', 'C', 'D', 'E', 'a', 'b', 'c', 'd', 'e']
+
+const str1 = '5';
+
+console.log(str1.padStart(5, '0'));
+// expected output: "05"
+
+const fullNumber = '2034399002125581';
+const last4Digits = fullNumber.slice(-4);
+const maskedNumber = last4Digits.padStart(fullNumber.length, '*');
+
+console.log(maskedNumber);
+// expected output: "************5581"
+
+'abc'.repeat(1)    // RangeError
+'abc'.repeat(0)     // ''
+'abc'.repeat(1)     // 'abc'
+'abc'.repeat(2)     // 'abcabc'
+'abc'.repeat(3.5)   // 'abcabcabc' (count will be converted to integer)
+'abc'.repeat(1)   // RangeError
+
+const p = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?';
+
+console.log(p.replace('dog', 'monkey'));
+// expected output: "The quick brown fox jumps over the lazy monkey. If the dog reacted, was it really lazy?"
+
+
+const regex = /Dog/i;
+console.log(p.replace(regex, 'ferret'));
+// expected output: "The quick brown fox jumps over the lazy ferret. If the dog reacted, was it really lazy?"
+const p = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?';
+
+console.log(p.replaceAll('dog', 'monkey'));
+// expected output: "The quick brown fox jumps over the lazy monkey. If the monkey reacted, was it really lazy?"
+
+
+// global flag required when calling replaceAll with regex
+const regex = /Dog/ig;
+console.log(p.replaceAll(regex, 'ferret'));
+// expected output: "The quick brown fox jumps over the lazy ferret. If the ferret reacted, was it really lazy?"
+
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+const words = str.split(' ');
+console.log(words[3]);
+// expected output: "fox"
+
+const chars = str.split("");
+console.log(chars[8]);
+console.log(str.split(""));
+// expected output: "k"
+
+const strCopy = str.split();
+console.log(strCopy);
+// expected output: Array ["The quick brown fox jumps over the lazy dog."]
